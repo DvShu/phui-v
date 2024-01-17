90bc33ca11f3556186705f9bcc581c9f4a7703ac
Tenny <joel.shu@qq.com>
2024-01-17 21:31:38 +0800

    feat: 文档构建
    Signed-off-by: Tenny <joel.shu@qq.com>

--- vite.config.ts
+++ vite.config.ts
@@ -5,27 +5,27 @@ import dts from "vite-plugin-dts";
 
 // https://vitejs.dev/config/
 export default defineConfig({
-	plugins: [
-		vue(),
-		dts({
-			copyDtsFiles: false,
-		}),
-	],
-	build: {
-		outDir: "lib",
-		lib: {
-			entry: path.resolve(__dirname, "src/index.ts"),
-			formats: ["es"],
-			fileName: () => {
-				return "index.js";
-			},
-		},
-		rollupOptions: {
-			// 确保外部化处理那些你不想打包进库的依赖
-			external: ["vue", "ph-utils/lib/validator", "ph-utils/lib/index"],
-		},
-	},
-	server: {
-		host: true,
-	},
+  plugins: [
+    vue(),
+    dts({
+      copyDtsFiles: false,
+    }),
+  ],
+  build: {
+    outDir: "es",
+    lib: {
+      entry: path.resolve(__dirname, "src/index.ts"),
+      formats: ["es"],
+      fileName: () => {
+        return "index.js";
+      },
+    },
+    rollupOptions: {
+      // 确保外部化处理那些你不想打包进库的依赖
+      external: ["vue", "ph-utils"],
+    },
+  },
+  server: {
+    host: true,
+  },
 });
